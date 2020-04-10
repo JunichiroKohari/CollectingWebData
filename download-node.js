3ce1bace7420d61e7027e9471e90feb7e734c8d2
JunichiroKohari <junichiro@kohari.jp>
2020-04-10 21:29:27 +0900

    関数化、青空文庫取得するもリダイレクトしろと言われる

--- download-node.js
+++ download-node.js
@@ -2,21 +2,35 @@
 
 // ダウンロード元URLの指定
 var url = 'http://kujirahand.com/';
-// 保存先のパスを指定
-var savepath = 'test.html';
+// ダウンロード
+download(
+    "http://www.aozora.gr.jp/index_pages/person81.html",
+    "miyazawakenji.html",
+    function () {
+        console.log("ok, kenji");
+    }
+);
+download(
+    "http://www.aozora.gr.jp/index_pages/person148.html",
+    "natumesoseki.html",
+    function () {
+        console.log("ok, soseki");
+    }
+);
 
-// 利用モジュールの取り込み
-var http = require('http');
-var fs = require('fs');
+function download(url, savepath, callback) {
+    // 利用モジュールの取り込み
+    var http = require('http');
+    var fs = require('fs');    
+    // 出力先を指定
+    var outfile = fs.createWriteStream(savepath);
 
-// 出力先を指定
-var outfile = fs.createWriteStream(savepath);
-
-// 非同期でURLからファイルをダウンロード
-http.get(url, function(res) {
-    res.pipe(outfile);
-    res.on('end', function() {
-        outfile.close();
-        console.log('ok(๑˃̵ᴗ˂̵)');
-    });
-})
+    // 非同期でURLからファイルをダウンロード
+    http.get(url, function(res) {
+        res.pipe(outfile);
+        res.on('end', function() {
+            outfile.close();
+            console.log('ok(๑˃̵ᴗ˂̵)');
+        });
+    })
+}
