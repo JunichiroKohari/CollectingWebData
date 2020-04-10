c030b42c451cda742725384c53350458e0994967
JunichiroKohari <junichiro@kohari.jp>
2020-04-10 21:48:49 +0900

    青空文庫の宮沢賢治のページ取得

---
+++ getpage.js
@@ -0,0 +1,15 @@
+var client = require('cheerio-httpcli');
+
+// ダウンロード
+var url = 'http://www.aozora.gr.jp/index_pages/person81.html';
+var param = {};
+
+client.fetch(url, param, function (err, $, res) {
+    if (err) {
+        console.error('error:', err);
+        return;
+    }
+    // ダウンロードした結果を画面に表示
+    var body = $.html();
+    console.log(body);
+});
